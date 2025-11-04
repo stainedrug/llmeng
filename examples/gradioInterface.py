b369bac3427f07468be3df701f8ba0873bc780bb
nuke <nunya@home.com>
2025-11-04 17:53:41 -0500

    Added Gradio Examples

---
+++ examples/gradioInterface.py
@@ -0,0 +1,265 @@
+import os
+import requests
+from dotenv import load_dotenv
+from openai import OpenAI
+import gradio as gr
+
+load_dotenv(override=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+google_api_key = os.getenv('GOOGLE_API_KEY')
+google_api_key = os.getenv('GOOGLE_API_KEY')
+
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+    
+if anthropic_api_key:
+    print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+else:
+    print("Anthropic API Key not set (and this is optional)")
+
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:2]}")
+else:
+    print("Google API Key not set (and this is optional)")
+
+# Connect to OpenAI client library
+# A thin wrapper around calls to HTTP endpoints
+
+openai = OpenAI()
+
+# For Gemini, DeepSeek and Groq, we can use the OpenAI python client
+# Because Google and DeepSeek have endpoints compatible with OpenAI
+# And OpenAI allows you to change the base_url
+
+anthropic_url = "https://api.anthropic.com/v1/"
+gemini_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+ollama_url = "http://localhost:11434/v1"
+
+anthropic = OpenAI(api_key=anthropic_api_key, base_url=anthropic_url)
+gemini = OpenAI(api_key=google_api_key, base_url=gemini_url)
+ollama = OpenAI(api_key="ollama", base_url=ollama_url)
+
+system_message = "You are a helpful assistant"
+
+def message_gpt(prompt):
+    messages = [{"role": "system", "content": system_message}, {"role": "user", "content": prompt}]
+    response = openai.chat.completions.create(model="gpt-4.1-mini", messages=messages)
+    return response.choices[0].message.content
+
+message_gpt("What is today's date?")
+
+def shout(text):
+    print(f"Shout has been called with input {text}")
+    return text.upper()
+
+#shout("hello")
+
+#gr.Interface(fn=shout, inputs="textbox", outputs="textbox", flagging_mode="never").launch()
+
+#gr.Interface(fn=shout, inputs="textbox", outputs="textbox", flagging_mode="never").launch(share=True)
+
+#Example with user pass this is for demonstration purposes, always follow the guidelines for security and privacy
+# can pass in multiple id&pass in tuples
+#gr.Interface(fn=shout, inputs="textbox", outputs="textbox", flagging_mode="never").launch(inbrowser=True, auth=("admin", "badpassword"))
+
+# Force Dark Mode
+force_dark_mode = """
+function refresh() {
+    const url = new URL(window.location);
+    if (url.searchParams.get('__theme') !== 'dark') {
+        url.searchParams.set('__theme', 'dark');
+        window.location.href = url.href;
+    }
+}
+"""
+#simple user pass interface
+#gr.Interface(fn=shout, inputs="textbox", outputs="textbox", flagging_mode="never", js=force_dark_mode).launch()
+
+# An example of an inpout box to use the shour function ( just repeats the input as caps)
+
+message_input = gr.Textbox(label="Your message:", info="Enter a message to be shouted", lines=7)
+message_output = gr.Textbox(label="Response:", lines=8)
+
+view = gr.Interface(
+    fn=shout,
+    title="Shout", 
+    inputs=[message_input], 
+    outputs=[message_output], 
+    examples=["hello", "howdy"], 
+    flagging_mode="never"
+    )
+#view.launch()
+
+#Now lets do something usefull and make an interface for chatgpt
+
+message_input = gr.Textbox(label="Your message:", info="Enter a message for GPT-4.1-mini", lines=7)
+message_output = gr.Textbox(label="Response:", lines=8)
+
+view = gr.Interface(
+    fn=message_gpt,
+    title="GPT", 
+    inputs=[message_input], 
+    outputs=[message_output], 
+    examples=["hello", "howdy"], 
+    flagging_mode="never"
+    )
+#view.launch()
+
+#Now lets have it respond in markdown
+
+system_message = "You are a helpful assistant that responds in markdown without code blocks"
+
+message_input = gr.Textbox(label="Your message:", info="Enter a message for GPT-4.1-mini", lines=7)
+message_output = gr.Markdown(label="Response:")
+
+view = gr.Interface(
+    fn=message_gpt,
+    title="GPT", 
+    inputs=[message_input], 
+    outputs=[message_output], 
+    examples=[
+        "Explain the Transformer architecture to a layperson",
+        "Explain the Transformer architecture to an aspiring AI engineer",
+        ], 
+    flagging_mode="never"
+    )
+#view.launch()
+
+#Funtion to stream results back with gpt
+def stream_gpt(prompt):
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": prompt}
+    ]
+    stream = openai.chat.completions.create(
+        model='gpt-4.1-mini',
+        messages=messages,
+        stream=True
+    )
+    result = ""
+    for chunk in stream:
+        result += chunk.choices[0].delta.content or ""
+        yield result
+
+def stream_claude(prompt):
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": prompt}
+    ]
+    stream = anthropic.chat.completions.create(
+        model='claude-sonnet-4-5-20250929',
+        messages=messages,
+        stream=True
+    )
+    result = ""
+    for chunk in stream:
+        result += chunk.choices[0].delta.content or ""
+        yield result
+
+
+
+message_input = gr.Textbox(label="Your message:", info="Enter a message for GPT-4.1-mini", lines=7)
+message_output = gr.Markdown(label="Response:")
+
+#lets use this function with GPT
+view = gr.Interface(
+    fn=stream_gpt,
+    title="GPT", 
+    inputs=[message_input], 
+    outputs=[message_output], 
+    examples=[
+        "Explain the Transformer architecture to a layperson",
+        "Explain the Transformer architecture to an aspiring AI engineer",
+        ], 
+    flagging_mode="never"
+    )
+#view.launch()
+message_input = gr.Textbox(label="Your message:", info="Enter a message for Claude 4.5 Sonnet", lines=7)
+message_output = gr.Markdown(label="Response:")
+
+#Funtion with Claude
+view = gr.Interface(
+    fn=stream_claude,
+    title="Claude", 
+    inputs=[message_input], 
+    outputs=[message_output], 
+    examples=[
+        "Explain the Transformer architecture to a layperson",
+        "Explain the Transformer architecture to an aspiring AI engineer",
+        ], 
+    flagging_mode="never"
+    )
+#view.launch()
+
+#function to stream results back using passed model so we can use a drop down in gradio 
+def stream_model(prompt, model):
+    if model=="GPT":
+        result = stream_gpt(prompt)
+    elif model=="Claude":
+        result = stream_claude(prompt)
+    else:
+        raise ValueError("Unknown model")
+    yield from result\
+    
+message_input = gr.Textbox(label="Your message:", info="Enter a message for the LLM", lines=7)
+model_selector = gr.Dropdown(["GPT", "Claude"], label="Select model", value="GPT")
+message_output = gr.Markdown(label="Response:")
+
+view = gr.Interface(
+    fn=stream_model,
+    title="LLMs", 
+    inputs=[message_input, model_selector], 
+    outputs=[message_output], 
+    examples=[
+            ["Explain the Transformer architecture to a layperson", "GPT"],
+            ["Explain the Transformer architecture to an aspiring AI engineer", "Claude"]
+        ], 
+    flagging_mode="never"
+    )
+#view.launch()
+
+#sample using Gradio with BASIC scraper
+from scraper import fetch_website_contents
+
+system_message = """
+You are an assistant that analyzes the contents of a company website landing page
+and creates a short brochure about the company for prospective customers, investors and recruits.
+Respond in markdown without code blocks.
+"""
+
+def stream_brochure(company_name, url, model):
+    yield ""
+    prompt = f"Please generate a company brochure for {company_name}. Here is their landing page:\n"
+    prompt += fetch_website_contents(url)
+    if model=="GPT":
+        result = stream_gpt(prompt)
+    elif model=="Claude":
+        result = stream_claude(prompt)
+    else:
+        raise ValueError("Unknown model")
+    yield from result
+
+name_input = gr.Textbox(label="Company name:")
+url_input = gr.Textbox(label="Landing page URL including http:// or https://")
+model_selector = gr.Dropdown(["GPT", "Claude"], label="Select model", value="GPT")
+message_output = gr.Markdown(label="Response:")
+
+view = gr.Interface(
+    fn=stream_brochure,
+    title="Brochure Generator", 
+    inputs=[name_input, url_input, model_selector], 
+    outputs=[message_output], 
+    examples=[
+            ["Cyberhoot", "https://cyberhoot.com", "GPT"],
+            ["Dirty Palms", "https://dirtypalms.com", "Claude"]
+        ], 
+    flagging_mode="never"
+    )
+#view.launch()
+
+
+
+# More Authentication and Gradio
